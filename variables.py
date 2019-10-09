48e3e75954639f68241344fca3598d3a0855090d
dprokofiev <dprokofiev@ya.ru>
2019-10-10 01:43:03 +0500

    init

---
+++ variables.py
@@ -0,0 +1,11 @@
+# object = computer
+# name - марка
+# price - цена
+# notebook - признак ноутбука (True - ноутбук)
+# age - возраст (месяцев)
+
+name = 'MacBook Pro'
+price = 2100.50
+notebook = True
+age = 1
+print(name, type(name), '\n', price, type(price), '\n', notebook, type(notebook), '\n', age, type(age))
